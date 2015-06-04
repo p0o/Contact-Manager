f2676c7855a10a5296440a704ed43e8f1f8958d1
Pooria Attarzadeh <pooria_az@ymail.com>
2015-06-04 23:15:44 +0430

    loading contacts names (just to know if flux works\!)

--- app/js/components/ContactList.react.js
+++ app/js/components/ContactList.react.js
@@ -2,8 +2,13 @@ var React = require('react');
 
 var ContactList = React.createClass({
 	render:function() {
+		var contacts=[];
+
+		for (contact in this.props.data) {
+			contacts.push(<li>{this.props.data[contact].name}</li>);
+		}
 		return(
-			<ul><li>Contact one</li></ul>
+			<ul>{contacts}</ul>
 		);
 	}
 });
