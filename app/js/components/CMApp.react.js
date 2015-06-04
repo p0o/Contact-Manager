0684832a4a5dde324d470bf62b97e6b8edbf5aca
Pooria Attarzadeh <pooria_az@ymail.com>
2015-06-04 23:12:52 +0430

    adding store connection

--- app/js/components/CMApp.react.js
+++ app/js/components/CMApp.react.js
@@ -8,15 +8,39 @@ var Navbar = require('./Navbar.react');
 var ContactList = require('./ContactList.react');
 var CMStore = require('../stores/CMStore');
 
+/**
+ * Retrieve the current Contacts data from the CMStore
+ */
+function getContactsState() {
+  return {
+    allContacts: CMStore.getAll()
+  };
+}
+
 var CMApp = React.createClass({
+	getInitialState: function() {
+    	return getContactsState();
+  	},
+  	componentDidMount: function() {
+		CMStore.addChangeListener(this._onChange);
+	},
+	componentWillUnmount: function() {
+		CMStore.removeChangeListener(this._onChange);
+	},
 	render: function() {
 		return(
 			<div>
 			  Contact Holder
-			  <Navbar />
-			  <ContactList />
+			  <Navbar/>
+			  <ContactList data={this.state.allContacts}/>
 			</div>
 		);
+	},
+	/**
+	* Event handler for 'change' events coming from the TodoStore
+	*/
+	_onChange: function() {
+		this.setState(getContactsState());
 	}
 });
 
